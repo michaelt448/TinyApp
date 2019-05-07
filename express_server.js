0e10e3251ce8397e81f9975551cc6d9f598f8c08
vagrant <mikegithub77@gmail.com>
2019-05-07 21:56:30 +0000

    added package to be able to post and added new route which takes in a value, also added a random string generator

--- express_server.js
+++ express_server.js
@@ -1,12 +1,26 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
 
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = function() {
+  let length = 6;
+  let word = '';
+  while(length > 0) {
+    let charCode = Math.floor(Math.random()*25 + 97);
+    word += String.fromCharCode(charCode);
+    length--;
+  }
+  return word;
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -23,10 +37,20 @@ app.get('/urls',(req,res) => {
   res.render('urls_index', templateVars);
 })
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get('/urls/:shortURL', (req,res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL : urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
