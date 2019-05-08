7aead29abae5abea50dba1b428aab32da4590e62
vagrant <mikegithub77@gmail.com>
2019-05-08 15:28:30 +0000

    clean up

--- express_server.js
+++ express_server.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require('express');
+const app = express();
+const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
 
-
-var urlDatabase = {
+//local dataBase
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Generates Random string from of length 6 from combination of 6 lower case letters
 const generateRandomString = function() {
   let length = 6;
   let word = '';
@@ -20,39 +21,56 @@ const generateRandomString = function() {
   return word;
 }
 
-
+//setUp
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set('view engine', 'ejs');
+//End setUp
 
+
+//Rootpage - prints hello in browser
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//hello extension - prints Hello world in browser
 app.get('/hello', (req,res)=> {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//GET - urls extension - brings to a table of urls with tinyURL on left and Full URL to right
 app.get('/urls',(req,res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 })
 
+// GET - page to make a new tinyURL for any URL
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 })
 
+// GET - sends user to URL using the current shortURL
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if(longURL === undefined) {
+    res.send("no such shortURL was found");
+  }
+  res.redirect(longURL);
+});
+
+// GET - brings to a page which shows the shortURL for the specific URL
 app.get('/urls/:shortURL', (req,res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL : urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 })
 
+// POST - uses a generated String as shortURL and stores the shortURL to a URL
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);
 });
 
+// Runs servers on PORT
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
